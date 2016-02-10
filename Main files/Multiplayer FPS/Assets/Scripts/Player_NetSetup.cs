38ed5f37edc5b2ad30c38499d69b83f21b68cb22
DrWallie <WallieWallex@hotmail.com>
2016-02-10 20:13:18 +0100

    summary
    description

---
+++ Main files/Multiplayer FPS/Assets/Scripts/Player_NetSetup.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetSetup : NetworkBehaviour
+{
+
+	[SerializeField] AudioListener audioListener;
+	[SerializeField] Camera PlayerCam;
+
+	[SerializeField] string remoteLayerName = "RemotePlayer";
+
+	void Start ()
+	{
+		if(!isLocalPlayer)
+		{
+			Disable();
+			AssignRemoteLayer();
+		}
+
+		RegisterPlayer();
+	}
+
+	void RegisterPlayer()
+	{
+		string _ID = "Player  " + GetComponent<NetworkIdentity>().netId;
+		transform.name = _ID;
+	}
+
+	void AssignRemoteLayer ()
+	{
+		gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+	}
+
+	void Disable ()
+	{
+		PlayerCam.enabled = false;
+		audioListener.enabled = false;
+		GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+		GetComponent<CharacterController>().enabled = false;
+	}
+}
